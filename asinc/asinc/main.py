1ff775951d3c1ce64e042b4ad1fb92467d60947a
Grigory-Yunusov <grigoriyzawr@gmail.com>
2024-02-19 19:23:33 +0300

    ready
    ready

--- asinc/asinc/main.py
+++ asinc/asinc/main.py
@@ -1,53 +1,27 @@
+import platform
 import aiohttp
 import asyncio
-import json
-from datetime import datetime, timedelta
 
 
-class ExchangeRateAPI:
-    API_URL = "https://api.privatbank.ua/#p24/exchangeArchive"
 
-    async def get_exchange_rates(self, start_date, end_date):
-        if (end_date - start_date).days > 10:
-            print("Помилка: Можна запитати курс валют не більше, ніж за останні 10 днів.")
-            return
 
-        params = {
-            "json": "",
-            "date": end_date.strftime("%d.%m.%Y")
-        }
 
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(self.API_URL, params=params) as response:
-                    data = await response.json()
-                    if response.status != 200 or "exchangeRate" not in data:
-                        print("Помилка: Неможливо отримати курс валют.")
-                        return
 
-                    exchange_rates = data["exchangeRate"]
-                    result = {}
+async def main():
 
-                    for rate in exchange_rates:
-                        if rate["currency"] in ["EUR", "USD"]:
-                            result[rate["currency"]] = rate["saleRateNB"]
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5') as response:
 
-                    return result
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+            print('Cookies: ', response.cookies)
+            print(response.ok)
+            result = await response.json()
+            return result
 
-            except aiohttp.ClientError as e:
-                print(f"Помилка мережі: {e}")
-
-def main():
-    start_date = datetime.now() - timedelta(days=10)
-    end_date = datetime.now()
-
-    api = ExchangeRateAPI()
-    loop = asyncio.get_event_loop()
-    exchange_rates = loop.run_until_complete(api.get_exchange_rates(start_date, end_date))
-
-    if exchange_rates:
-        print(f"Курс EUR: {exchange_rates.get('EUR')}")
-        print(f"Курс USD: {exchange_rates.get('USD')}")
 
 if __name__ == "__main__":
-    main()
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    r = asyncio.run(main())
+    print(r)
---
+++ asinc/asinc/rezerv.py
@@ -0,0 +1,53 @@
+import aiohttp
+import asyncio
+import json
+from datetime import datetime, timedelta
+
+
+class ExchangeRateAPI:
+    API_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+
+    async def get_exchange_rates(self, start_date, end_date):
+        if (end_date - start_date).days > 10:
+            print("Помилка: Можна запитати курс валют не більше, ніж за останні 10 днів.")
+            return
+
+        params = {
+            "json": "",
+            "date": end_date.strftime("%d.%m.%Y")
+        }
+
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(self.API_URL, params=params) as response:
+                    data = await response.json()
+                    if response.status != 200 or "exchangeRate" not in data:
+                        print("Помилка: Неможливо отримати курс валют.")
+                        return
+
+                    exchange_rates = data["exchangeRate"]
+                    result = {}
+
+                    for rate in exchange_rates:
+                        if rate["currency"] in ["EUR", "USD"]:
+                            result[rate["currency"]] = rate["saleRateNB"]
+
+                    return result
+
+            except aiohttp.ClientError as e:
+                print(f"Помилка мережі: {e}")
+
+def main():
+    start_date = datetime.now() - timedelta(days=10)
+    end_date = datetime.now()
+
+    api = ExchangeRateAPI()
+    loop = asyncio.get_event_loop()
+    exchange_rates = loop.run_until_complete(api.get_exchange_rates(start_date, end_date))
+
+    if exchange_rates:
+        print(f"Курс EUR: {exchange_rates.get('EUR')}")
+        print(f"Курс USD: {exchange_rates.get('USD')}")
+
+if __name__ == "__main__":
+    main()
